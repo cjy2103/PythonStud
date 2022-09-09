e9df6b82a8b2d522e1d8812a6b721db78144e795
cjy2103 <cjy2103@naver.com>
2022-09-10 05:40:57 +0900

    numpy 슬라이싱 해야함

--- blogPosting/Library/Numpy/Main.py
+++ blogPosting/Library/Numpy/Main.py
@@ -23,3 +23,37 @@ print(np.arange(5))
 
 # 1씩 증가하는 배열 생성 ( 시작위치 2)
 print(np.arange(2, 10))
+
+# zero 행렬 생성
+print(np.zeros((3, 3)))
+
+# 유닛 행렬
+print(np.ones((2, 4)))
+
+# 모든 요소가 7인 2 * 4 행렬
+print(np.full((2, 4), 7))
+
+# 단위 행렬
+print(np.eye(3))
+
+# 배열의 차원 변환
+a = np.arange(12)
+print(a)
+
+b = a.reshape((4, 3))
+print(b)
+
+lst = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+]
+
+arr = np.array(lst)
+
+a = arr[0:3, 0:3]
+print(a)
+
+a = arr[1:, 1:]
+print(a)
