13a6c374345ee676ea108d5cbe062d2012df7ad8
cjy2103 <cjy2103@naver.com>
2022-09-10 01:55:31 +0900

    Munpy 공부중

--- blogPosting/Library/Numpy/Main.py
+++ blogPosting/Library/Numpy/Main.py
@@ -1,1 +1,25 @@
-print("Numpy 준비")
+import numpy as np
+
+# 리스트에서 행렬 생성
+data = [[1, 2, 3], [4, 5, 6]]
+
+print(data)
+
+arr = np.array(data)
+
+print(arr)
+
+# 배열의 열수
+print(arr.ndim)
+
+# 배열의 차원
+print(arr.shape)
+
+# 배열의 원소 접근
+print(arr[0][0])
+
+# 1씩 증가하는 배열 생성
+print(np.arange(5))
+
+# 1씩 증가하는 배열 생성 ( 시작위치 2)
+print(np.arange(2, 10))
