e2efb310229f4d04cb081a419eceffb8813e24da
jy.choi <cjy2103@naver.com>
2022-09-19 17:11:20 +0900

    Python 함수선언

---
+++ blogPosting/basic/Function/Main.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    print("입력받은값:", a, b)
+    c = a + b
+    print("두수 더한값:", c)
+    return c
+
+
+def test():
+    print("함수 테스트")
+
+
+result = add(5, 4)
+
+print(result)
+
+test()
+
