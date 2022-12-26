d4b52146a3ecae535a92cfe4e62033e89eba4512
jy.choi <cjy2103@naver.com>
2022-12-26 12:26:52 +0900

    텍스트 파일 음성으로 변환하기 포스팅 완

--- blogPosting/Program/TextToSpeek/main.py
+++ blogPosting/Program/TextToSpeek/main.py
@@ -1,9 +1,23 @@
 from gtts import gTTS
 from playsound import playsound
+import os
 
-
+path = os.path.dirname(os.path.abspath(__file__))
 text = "음성 파일 테스트 예제 입니다."
 
 tts = gTTS(text=text, lang="ko")
-tts.save(r"음성파일\test.mp3")
+tts.save(r"soundfile\test.mp3")
+
+# playsound(path+r"\soundfile\test.mp3")
+
+file_path = path+r"\text\text_file"
+
+with open(file_path, "rt", encoding="UTF8") as f:
+    read_file = f.read()
+
+tfts = gTTS(text=read_file, lang="ko")
+tfts.save(r"soundfile\Text.mp3")
+
+playsound(path+r"\soundfile\Text.mp3")
+
 
